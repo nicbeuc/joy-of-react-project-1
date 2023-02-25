293963c91d7aef53dd2005645a90db7ba2f84347
nicbeuc <89465306+NicBeucM3@users.noreply.github.com>
2023-02-25 12:36:51 -0500

    some refactoring

--- src/components/Game/Guess.js
+++ src/components/Game/Guess.js
@@ -1,14 +1,12 @@
 import { checkGuess } from "../../game-helpers";
 import { range } from "../../utils";
 
-const letterRange = range(0, 5);
-
 function Guess({ guess, answer }) {
   if (guess) {
     const letterStatuses = checkGuess(guess, answer);
     return (
       <p className="guess">
-        {letterRange.map(index => (
+        {range(5).map(index => (
           <span
             key={index}
             className={`cell ${letterStatuses[index].status}`}
@@ -21,7 +19,7 @@ function Guess({ guess, answer }) {
   }
   return (
     <p className="guess">
-      {letterRange.map(index => (
+      {range(5).map(index => (
         <span
           key={index}
           className="cell"
--- src/components/Game/GuessResults.js
+++ src/components/Game/GuessResults.js
@@ -1,14 +1,11 @@
 import Guess from "./Guess";
 import { range } from "../../utils";
-
 import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
-
-const guessRange = range(0, NUM_OF_GUESSES_ALLOWED);
 
 function GuessResults({ guesses, answer }) {
   return (
     <div className="guess-results">
-      {guessRange.map(index => (
+      {range(NUM_OF_GUESSES_ALLOWED).map(index => (
         <Guess
           key={index}
           guess={guesses[index]}
